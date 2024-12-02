98cbc66b39cbdc717b2e29feb8bfc22378103c8d
SinellaCorinne <sinelladk12@gmail.com>
2024-12-02 15:51:29 +0100

    Interfaces de plantes

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+import Cart from './Cart'
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
+import '../styles/Layout.css'
+
+function App() {
+    // Initialiser le panier avec les données de localStorage
+    const [cart, updateCart] = useState(() => {
+        const savedCart = localStorage.getItem('cart')
+        return savedCart ? JSON.parse(savedCart) : []
+    })
+
+    return (
+        <div>
+            <Banner>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1 className='lmj-title'>La maison jungle</h1>
+            </Banner>
+            <div className='lmj-layout-inner'>
+                <Cart cart={cart} updateCart={updateCart} />
+                <ShoppingList cart={cart} updateCart={updateCart} />
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Cart.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+import '../styles/Cart.css'
+
+function Cart({ cart, updateCart }) {
+	const [isOpen, setIsOpen] = useState(true)
+	
+    // Sauvegarder le panier dans localStorage à chaque modification
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
+
+    // Calculer le total
+    const total = Array.isArray(cart) ? cart.reduce(
+        (acc, plantType) => acc + plantType.amount * plantType.price,
+        0
+    ) : 0
+
+    // Afficher une alerte avec le total à payer
+    useEffect(() => {
+        alert(`J'aurai ${total}€ à payer 💸`)
+    }, [total])
+
+
+	return isOpen ? (
+		<div className='lmj-cart'>
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(false)}
+			>
+				Fermer
+			</button>
+			{cart.length > 0 ? (
+				<div>
+					<h2>Panier</h2>
+					<ul>
+						{cart.map(({ name, price, amount }, index) => (
+							<div key={`${name}-${index}`}>
+								{name} {price}€ x {amount}
+							</div>
+						))}
+					</ul>
+					<h3>Total :{total}€</h3>
+					<button onClick={() => updateCart([])}>Vider le panier</button>
+				</div>
+			) : (
+				<div>Votre panier est vide</div>
+			)}
+		</div>
+	) : (
+		<div className='lmj-cart-closed'>
+			<button
+				className='lmj-cart-toggle-button'
+				onClick={() => setIsOpen(true)}
+			>
+				Ouvrir le Panier
+			</button>
+		</div>
+	)
+}
+
+export default Cart
